fb6ff5b70483b5dbde7bfa5f8e00fcbfed2f304d
Jacob Firek <jacob@interlay.io>
2023-04-13 17:33:08 -0400

    lgtm

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    navLinks.forEach((link) => {
+        if (link.href === window.location.href) {
+            link.classList.add("active");
+        }
+    });
+});
